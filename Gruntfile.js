09a86b0ff92eded1747f0b27a0a07e5b08819e64
Mike Conley <mike.d.conley@gmail.com>
2014-08-06 12:51:01 -0400

    Grunt deployment works now.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        rsync: {
+            options: {
+                args: ['-avz', '--verbose', '--delete'],
+                recursive: true
+            },
+            prod: {
+                options: {
+                    // the dir you want to sync, in this case the current dir
+                    src: './_site/',
+                    // where should it be synced to on the remote host?
+                    dest: '~/public_html/bugnotes/',
+                    // what's the creds and host
+                    host: 'mconley2@people.mozilla.com'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-rsync');
+
+    grunt.registerTask('deploy', ['rsync:prod']);
+};
+
